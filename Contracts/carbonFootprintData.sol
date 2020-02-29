e3ac6aac1883ec92ff8aa8cd9ad56ba7d9ebf3b2
Raphael Benoit <raphmc@hotmail.com>
2020-02-29 01:13:34 +0000

    added gitignore and updated contracts

--- Contracts/carbonFootprintData.sol
+++ Contracts/carbonFootprintData.sol
@@ -1,24 +1,39 @@
-pragma solidity ^0.6.1;
+pragma solidity ^0.6.0;
 
-contract CarbonFootprintData {
+import "@openzeppelin/ownership/Ownable.sol";
+import "@openzeppelin/math/SafeMath.sol";
+
+contract CarbonFootprintData is Ownable {
     
     // DAO contract address
     address owner;
+
+    uint256 co2kenPrice = 10e18;      // Price in DAI with 18 decimals
     
     // https://digiconomist.net/ethereum-energy-consumption
-    uint txCarbonFootprint = 31000000;    // mWh per Ethereum TX (1000 milliWattHours = 1 Wh)
+    uint256 txCarbonFootprint = 31000000;    // mWh per Ethereum TX (1000 milliWattHours = 1 Wh)
     
     // https://hackernoon.com/green-smart-contracts-theres-more-to-blockchain-energy-consumption-than-consensus-898fb23eea75
     // https://twitter.com/CryptoDemetrius/status/1144357399744196609
-    uint gasCarbonFootprint = 400;      // mWh per Ethereum TX (1000 milliWattHours = 1 Wh)
+    uint256 gasCarbonFootprint = 400;      // mWh per Ethereum TX (1000 milliWattHours = 1 Wh)
+
+    event CO2kenPriceChanged(uint256 newPrice);
+    event TxCarbonFootprintChanged(uint256 newTxCarbonFootprint);
+    event GasCarbonFootprintChanged(uint256 newGasCarbonFootprint);
     
-    
-    function setTxCarbonFootprint(uint _txCarbonFootprint) public {
+    function setCO2kenPrice(uint _co2kenPrice) public onlyOwner() {
+        co2kenPrice = _co2kenPrice;
+        emit CO2kenPriceChanged(_co2kenPrice);
+    }
+
+    function setTxCarbonFootprint(uint _txCarbonFootprint) public onlyOwner() {
         txCarbonFootprint = _txCarbonFootprint;
+        emit TxCarbonFootprintChanged(_txCarbonFootprint);
     }
     
-    function setgasCarbonFootprint(uint _gasCarbonFootprint) public {
+    function setgasCarbonFootprint(uint _gasCarbonFootprint) public onlyOwner() {
         gasCarbonFootprint = _gasCarbonFootprint;
+        emit GasCarbonFootprintChanged(_gasCarbonFootprint);
     }
     
     
