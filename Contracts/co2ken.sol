b01eb25fd68d587697e5ad20d34345898248e491
0xEther <60259703+0xEther@users.noreply.github.com>
2020-02-29 01:15:14 +0000

    Update co2ken.sol

--- Contracts/co2ken.sol
+++ Contracts/co2ken.sol
@@ -3,39 +3,53 @@ pragma solidity ^0.6.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/ownership/Ownable.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/math/SafeMath.sol";
 
+abstract contract DaiLike {
+    function transferFrom(address src, address dst, uint wad) public virtual returns (bool);
+    function transfer(address dst, uint wad) external virtual returns (bool);
+    function approve(address usr, uint wad) external virtual returns (bool);
+}
+
 contract CO2KEN is Ownable {
     string private _name;
     string private _symbol;
     uint8 private _decimals;
     
+    DaiLike daiToken;
+    
     using SafeMath for uint256;
 
-    uint256 balance;
-    uint256 private _totalSupply;
+    uint256 public balance;
     
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Burn(address indexed from, uint256 value);
+    event Mint(uint256 value);
 
-    constructor(string memory name, string memory symbol, uint8 decimals) public {
+    constructor(address _daiToken, string memory name, string memory symbol, uint8 decimals) public {
         _name = name;
         _symbol = symbol;
         _decimals = decimals;
-    }
-
-    function totalSupply() public view returns (uint256) {
-        return _totalSupply;
+        // interface with dai token
+        daiToken = DaiLike(_daiToken);
     }
     
     function mint(uint256 amount) public onlyOwner() {
-        _totalSupply = _totalSupply.add(amount);
         balance = balance.add(amount);
-        emit Transfer(address(0), owner(), amount);
+        emit Mint(amount);
     }
     
-    function burn(uint256 amount) public onlyOwner() {
-        balance = balance.sub(amount, "ERC20: burn amount exceeds balance");
-        _totalSupply = _totalSupply.sub(amount);
-        emit Transfer(owner(), address(0), amount);
+    function approve() public {
+        daiToken.approve(address(this), uint(-1));
+    }
+    
+    function burn() public {
+        // @TODO require user has enough dai
+        
+        // @TODO calculate the amount to burn
+        uint256 burnAmount = 5000000000000000000;
+        
+        balance = balance.sub(burnAmount, "ERC20: burn amount exceeds balance");
+        // @TODO end dai to owner
+        // daiToken.transferFrom(msg.sender, owner(), _wad);
+        emit Burn(_msgSender(), burnAmount);
     }
     
     function transferOwnership(address newOwner) public virtual override onlyOwner {
