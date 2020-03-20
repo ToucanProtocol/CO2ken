bad40f144b17af38b29d749cd731109acc59e199
0xEther <60259703+0xEther@users.noreply.github.com>
2020-03-20 17:05:42 +0000

    Added mul()

--- Contracts/co2ken.sol
+++ Contracts/co2ken.sol
@@ -42,6 +42,19 @@ contract CO2ken is Ownable {
     function rmul(uint256 x, uint256 y) internal pure returns (uint256 z) {
         z = mul(x, y) / ONE;
     }
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
 
     constructor(address storageTarget, address daiTarget, string memory name, string memory symbol, uint8 decimals) public {
         // set our token detail
