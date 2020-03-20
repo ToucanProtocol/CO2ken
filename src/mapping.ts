ad771c0f741e61a7b72c6d45a2d241ed4e97b97a
benesjan <janbenes1234@gmail.com>
2020-03-20 21:51:05 +0100

    Computing the overall number of offsetted tokens and current token balance using the graph

--- src/mapping.ts
+++ src/mapping.ts
@@ -6,27 +6,65 @@ import {
     OwnershipTransferred,
     Withdrawal
 } from "../generated/CO2ken/CO2ken"
-import {UserBalance} from "../generated/schema"
+import {UserBalance, ContractBalance} from "../generated/schema"
 
 export function handleCarbonOffsetted(event: CarbonOffsetted): void {
-    let id = event.params.from.toHex()
-    let userBalance = UserBalance.load(id)
+    let newlyOffsetted = event.params.value
+
+    let userId = event.params.from.toHex()
+    let contractId = event.address.toHex()
+
+    let userBalance = UserBalance.load(userId)
+    let contractBalance = ContractBalance.load(contractId)
+
     if (userBalance == null) {
-        userBalance = new UserBalance(id)
+        userBalance = new UserBalance(userId)
         // TODO: check if the capacity is ok
         userBalance.balance = new BigInt(1024)
-        log.info("Creating new UserBalance entity for address {}", [id])
+        log.info("Creating new UserBalance entity for address {}", [userId])
     }
-    userBalance.balance = userBalance.balance.plus(event.params.value)
+
+    if (contractBalance == null) {
+        contractBalance = new ContractBalance(contractId)
+        // TODO: check if the capacity is ok
+        contractBalance.available = new BigInt(1024)
+        contractBalance.offsetted = new BigInt(1024)
+        log.info("Creating new ContractBalance entity for address {}", [contractId])
+    }
+
+    userBalance.balance = userBalance.balance.plus(newlyOffsetted)
     userBalance.save()
     log.info("User balance was increased by {} tokens. Resulting user balance: {}",
-        [event.params.value.toString(), userBalance.balance.toString()])
+        [newlyOffsetted.toString(), userBalance.balance.toString()])
+
+    contractBalance.available = contractBalance.available.minus(newlyOffsetted)
+    contractBalance.offsetted = contractBalance.offsetted.plus(newlyOffsetted)
+    contractBalance.save()
 }
 
 export function handleDebug(event: Debug): void {
 }
 
 export function handleMinted(event: Minted): void {
+    let newlyMinted = event.params.tokensMinted
+
+    let contractId = event.address.toHex()
+
+    let contractBalance = ContractBalance.load(contractId)
+
+    if (contractBalance == null) {
+        contractBalance = new ContractBalance(contractId)
+        // TODO: check if the capacity is ok
+        contractBalance.available = new BigInt(1024)
+        contractBalance.offsetted = new BigInt(1024)
+        log.info("Creating new ContractBalance entity for address {}", [contractId])
+    }
+
+    log.info("Available token balance was increased by {} tokens. Resulting user balance: {}",
+        [newlyMinted.toString(), contractBalance.available.toString()])
+
+    contractBalance.available = contractBalance.available.plus(newlyMinted)
+    contractBalance.save()
 }
 
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {
